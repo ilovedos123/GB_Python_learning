9d385c5c19f5bfa9b45767d9c395fa99061021e7
ilovedos <bs193@mail.ru>
2022-09-23 19:07:40 +0300

    added seminar_1 Classwork_upd

--- Seminar_1/Classwork/Classwork_4.py
+++ Seminar_1/Classwork/Classwork_4.py
@@ -3,4 +3,13 @@
 #      - 6,78 -> 7
 #     - 5 -> нет
 #     - 0,34 -> 3
+number = (input('Введите число: '))
+dot = '.'
+for i in range(len(number)):
+    if number[i] == dot:
+        print(number[i+1])
+if dot not in number:
+    print('нет')
+
+
 
