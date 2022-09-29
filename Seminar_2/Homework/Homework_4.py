28b8b659476d43b96f7b7cad3d98c6babee408d6
ilovedos <bs193@mail.ru>
2022-09-29 22:12:21 +0300

    added Seminar_2 Homework

--- Seminar_2/Homework/Homework_4.py
+++ Seminar_2/Homework/Homework_4.py
@@ -1,3 +1,23 @@
 # 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
+from random import randint
+n = int(input('Введите число : '))
+list = []
+for i in range(n):
+    list.append(randint(-n, n))  #формируем список list
+file = open('file.txt', 'r')     #выгружаем из файла позиции элементов в список position_list
+position_list = []
+for line in file:
+    position = int(line.strip())
+    position_list.append(position)
+result = 1                      #вычисляем произведение элементов списка list на позициях position_list
+for j in range(len(list)):
+    for k in range(len(position_list)):
+        if j == position_list[k]:
+            result = result * list[j]
+print(f'Произведение элементов на позициях из файла равно {result}')
+print('-------------')
+print(f'Список из N элементов, заполненных числами из промежутка [-N, N]: \n {list}')
+print('-------------')
+print(f'Позиции элементов для вычисления произведения: \n {position_list}')
--- Seminar_2/Homework/Homework_5.py
+++ Seminar_2/Homework/Homework_5.py
@@ -1,1 +1,14 @@
 # 5. Реализуйте алгоритм перемешивания списка.
+from random import randint
+list = [15, 0, -10, 5, -3, 2, -5, -3, 0, -15, 1, 15, -9, -7, -15]
+index_list = []
+while len(list) != len(index_list):  # Создаём массив произвольных, неповторяющихся индексов
+    index = randint(0, len(list) - 1)
+    if index in index_list:
+        continue
+    index_list.append(index)
+result_list = []
+for i in range(len(list)):                      # Заполняем итоговый массив значениями из списка list с индексами из списка index_list
+    result_list.append(list[index_list[i]])
+print(result_list)
+
