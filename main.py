89abaa3709904c6a80cfb9bfd170633a1b4810ba
ilovedos <bs193@mail.ru>
2023-06-08 16:03:38 +0300

    added Seminar_5 Homework

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-# -*- mode: python ; coding: utf-8 -*-
-import telebot
-from telebot import types
-
-
-bot = telebot.TeleBot('5769693306:AAHDPx-_pcLREaLJOv48PoMpwmjToRQA_L8')
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, message.from_user.first_name)
-
-bot.polling(none_stop=True, interval=0)
+a = -15
+b = 4
+c = a - b
+if c < 0:
+    c = -c
+print(c)
